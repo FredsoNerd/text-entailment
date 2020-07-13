3c5e7ea4f316b895f027cc75a1d288f4e1f0b461
Fredson <fredsonerd@hotmail.com>
2020-07-13 15:03:59 -0300

    solving free variables

--- demo.py
+++ demo.py
@@ -6,23 +6,72 @@ sys.setrecursionlimit(2000)
 
 # https://rdflib.readthedocs.io/en/stable/index.html
 
-def rdfterm_to_str(x):
+def rdfterm_to_str(x, prefix):
+    """
+    return the correct query term to mount an sparql
+    query. Terms of form "<[prefix](x|e)[n][suffix]>"
+    are formed as free terms like "?(x|e)[n][suffix]"
+    """
+    
+    if x.startswith(prefix): return "?"+x[len(prefix)::]
     return x.n3()
 
-def entail(graphH, graphT):
+def make_pre_sparql(graphT):
     """
-    parse indetifiers in graphT into valid RDF triples
-    and mount the SPARQL query to be queried in graphH
+    parse indetifiers in graphT into triples and mount
+    a pre-SPARQL query to be queried in ASK or SELECT
+
+    returns: a pre-SPARQL query format of graphT like:
+        <URI> <URI> ?var1 .
+        <URI> ?var2 <URI> .
+        ...
+        ?varn ?var1 <URI> .
     """
-    rdf = """"""
+
+    pre_sparql = """"""
+    prefix = "http://example.com/"
     for s,p,o in graphT:
-        rdf += rdfterm_to_str(s) + " "
-        rdf += rdfterm_to_str(p) + " "
-        rdf += rdfterm_to_str(o) + " .\n"
+        pre_sparql += rdfterm_to_str(s, prefix) + " "
+        pre_sparql += rdfterm_to_str(p, prefix) + " "
+        pre_sparql += rdfterm_to_str(o, prefix) + " .\n"
 
-    sparql_query = "ask{" + rdf + "}"
+    return pre_sparql
 
-    return bool(graphH.query(sparql_query))
+def entail_model(graphH, graphT):
+    """
+    receives a pair of HIPOTESIS and TEXT in RDF format
+    and try finding a model showing graphT as a subgraph
+    of graphH
+
+    returns: a ResultRow containing a list of models
+    """
+
+    pre_sparql = make_pre_sparql(graphT)
+    sparql = "select * where {" + pre_sparql +"}"
+
+    res = graphH.query(sparql)
+    print("Found {} models.".format(len(res)))
+    return res
+
+def entail_ask(graphH, graphT):
+    """
+    receives a pair of HIPOTESIS and TEXT in RDF format
+    and try finding a model showing graphT as a subgraph
+    of graphH; in this case, returns true
+
+    returns: true (entail) or false (not entail)
+    """
+
+    pre_sparql = make_pre_sparql(graphT)
+    sparql = "ask {" + pre_sparql +"}"
+
+    res = graphH.query(sparql)
+    return bool(res)
+
+#############################################
+# the graphs 0 and 1 were generated separtely
+# for the same phrase "Two dogs are fighting"
+# graphs are represented differently in 3.nq
 
 data = open("demos/3.nq", "rb")
 
@@ -30,12 +79,11 @@ g = ConjunctiveGraph()
 g.parse(data, format="nquads")
 graphs = [x for x in g.store.contexts()]
 
-# the graphs 0 and 1 were generated separtely
-# for same phrase "Two dogs are fighting"
-res = entail(graphs[0], graphs[1])
-print("Entail value: ", res) # false
-
-## we entail each phrase with itself
-# for i in range(98):
-    # res = entail(graphs[i-1], graphs[i-1])
-    # print("Graph {} entail value: ".format(i+1), res)
+# ask if there is a model
+res = entail_ask(graphs[0], graphs[1])
+print("Entail: ", res)
+# ask for model granting entail
+res = entail_model(graphs[0], graphs[1])
+print("Model: ", res)
+# iter over entail model showing model
+for model in res: pprint.pprint(model)
